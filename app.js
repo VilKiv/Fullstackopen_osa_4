b008201aeee706e4147c845f419ea88671282ef8
Ville <ville.kivioja@tuni.fi>
2024-12-30 17:29:25 +0200

    Tehty 4.1 ja 4.2

---
+++ app.js
@@ -0,0 +1,16 @@
+const config = require('./utils/config')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('',blogsRouter)
+module.exports = app 
